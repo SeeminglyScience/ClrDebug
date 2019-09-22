af9f40d76fb29801283d50fb3a789de5842edc58
Patrick Meinecke <seeminglyscience@gmail.com>
2019-09-22 17:16:28 -0400

    Fix mscordbi loading from wrong location

--- src/ClrDebug/MscordbiShim.cs
+++ src/ClrDebug/MscordbiShim.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using System.ComponentModel;
+using System.IO;
 using System.Runtime.InteropServices;
 
 namespace ClrDebug
 {
     internal static class MscordbiShim
     {
+        private unsafe delegate int CoreCLRCreateCordbObjectInvoker(
+                int iDebuggerVersion,
+                uint pid,
+                void* hmodTargetCLR,
+                void*** ppCordb);
+
+        private unsafe delegate int CreateCordbObjectInvoker(int iDebuggerVersion, void*** ppCordb);
+
+        private const string CoreCLRCreateCordbObjectName = "CoreCLRCreateCordbObject";
+
+        private const string CreateCordbObjectName = "CreateCordbObject";
+
         private static readonly IPlatformSpecificOps s_platform;
 
         static MscordbiShim()
@@ -13,21 +27,21 @@ namespace ClrDebug
             {
                 if (RuntimeInformation.FrameworkDescription.Contains("Core"))
                 {
-                    s_platform = new WindowsCoreOps();
+                    s_platform = new WindowsCorePlatform();
                     return;
                 }
 
-                s_platform = new WindowsDesktopOps();
+                s_platform = new WindowsDesktopPlatform();
                 return;
             }
 
             if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
-                s_platform = new MacOps();
+                s_platform = new MacPlatform();
                 return;
             }
 
-            s_platform = new UnixOps();
+            s_platform = new UnixPlatform();
         }
 
         public static string ClrModuleName => s_platform.ClrModuleName;
@@ -57,78 +71,156 @@ namespace ClrDebug
                 void*** ppCordb);
         }
 
-        private class UnixOps : IPlatformSpecificOps
+        private abstract class PlatformBase<TInvoker> : IPlatformSpecificOps where TInvoker : Delegate
         {
-            public string ClrModuleName => "libcoreclr.so";
+            protected readonly TInvoker Invoker;
 
-            public unsafe void CreateCordbObject(int iDebuggerVersion, uint pid, void* hmodTargetCLR, void*** ppCordb)
+            public PlatformBase()
             {
-                NonWindowsInterop.CoreCLRCreateCordbObject(iDebuggerVersion, pid, hmodTargetCLR, ppCordb)
-                    .MaybeThrowHr();
+                string libPath = Path.Combine(
+                    Path.GetDirectoryName(typeof(int).Assembly.Location),
+                    DllName);
+
+                if (!File.Exists(libPath))
+                {
+                    throw new InvalidOperationException("Could not find mscordbi library.");
+                }
+
+                IntPtr module = GetLibrary(libPath);
+                if (module == IntPtr.Zero)
+                {
+                    throw new Win32Exception(Marshal.GetLastWin32Error());
+                }
+
+                IntPtr functionPtr = GetFunctionPointer(
+                    module,
+                    FunctionName);
+
+                if (functionPtr == IntPtr.Zero)
+                {
+                    throw new Win32Exception(Marshal.GetLastWin32Error());
+                }
+
+                Invoker = Marshal.GetDelegateForFunctionPointer<TInvoker>(functionPtr);
             }
-        }
 
-        private class MacOps : IPlatformSpecificOps
-        {
-            public string ClrModuleName => "libcoreclr.dylib";
+            public abstract string ClrModuleName { get; }
 
-            public unsafe void CreateCordbObject(int iDebuggerVersion, uint pid, void* hmodTargetCLR, void*** ppCordb)
-            {
-                NonWindowsInterop.CoreCLRCreateCordbObject(iDebuggerVersion, pid, hmodTargetCLR, ppCordb)
-                    .MaybeThrowHr();
-            }
-        }
+            public abstract string FunctionName { get; }
 
-        private class WindowsDesktopOps : IPlatformSpecificOps
-        {
-            public string ClrModuleName => "clr.dll";
+            public abstract string DllName { get; }
 
-            public unsafe void CreateCordbObject(int iDebuggerVersion, uint pid, void* hmodTargetCLR, void*** ppCordb)
-            {
-                WindowsDesktopInterop.CreateCordbObject(
-                    Math.Min(iDebuggerVersion, 4),
-                    ppCordb)
-                    .MaybeThrowHr();
-            }
-        }
-
-        private class WindowsCoreOps : IPlatformSpecificOps
-        {
-            public string ClrModuleName => "coreclr.dll";
-
-            public unsafe void CreateCordbObject(int iDebuggerVersion, uint pid, void* hmodTargetCLR, void*** ppCordb)
-            {
-                WindowsCoreInterop.CoreCLRCreateCordbObject(iDebuggerVersion, pid, hmodTargetCLR, ppCordb)
-                    .MaybeThrowHr();
-            }
-        }
-
-        private static class WindowsDesktopInterop
-        {
-            [DllImport("mscordbi", SetLastError = true)]
-            public static extern unsafe int CreateCordbObject(
-                int iDebuggerVersion,
-                void*** ppCordb);
-        }
-
-        private static class WindowsCoreInterop
-        {
-            [DllImport("mscordbi", SetLastError = true)]
-            public static extern unsafe int CoreCLRCreateCordbObject(
+            public abstract unsafe void CreateCordbObject(
                 int iDebuggerVersion,
                 uint pid,
                 void* hmodTargetCLR,
                 void*** ppCordb);
+
+            public abstract IntPtr GetLibrary(string libraryPath);
+
+            public abstract IntPtr GetFunctionPointer(IntPtr module, string functionName);
+        }
+
+        private abstract class WindowsPlatform<TInvoker> : PlatformBase<TInvoker>
+            where TInvoker : Delegate
+        {
+            public override string DllName => "mscordbi.dll";
+
+            public override IntPtr GetFunctionPointer(IntPtr module, string functionName)
+            {
+                return WindowsInterop.GetProcAddress(module, functionName);
+            }
+
+            public override IntPtr GetLibrary(string libraryPath)
+            {
+                return WindowsInterop.LoadLibraryW(libraryPath);
+            }
+        }
+
+        private class WindowsDesktopPlatform : WindowsPlatform<CreateCordbObjectInvoker>
+        {
+            public override string ClrModuleName => "clr.dll";
+
+            public override string FunctionName => CreateCordbObjectName;
+
+            public override unsafe void CreateCordbObject(int iDebuggerVersion, uint pid, void* hmodTargetCLR, void*** ppCordb)
+            {
+                Invoker(Math.Min(iDebuggerVersion, 4), ppCordb).MaybeThrowHr();
+            }
+        }
+
+        private class WindowsCorePlatform : WindowsPlatform<CoreCLRCreateCordbObjectInvoker>
+        {
+            public override string ClrModuleName => "coreclr.dll";
+
+            public override string FunctionName => CoreCLRCreateCordbObjectName;
+
+            public override unsafe void CreateCordbObject(
+                int iDebuggerVersion,
+                uint pid,
+                void* hmodTargetCLR,
+                void*** ppCordb)
+            {
+                Invoker(iDebuggerVersion, pid, hmodTargetCLR, ppCordb).MaybeThrowHr();
+            }
+        }
+
+        private abstract class NonWindowsPlatform : PlatformBase<CoreCLRCreateCordbObjectInvoker>
+        {
+            public override string FunctionName => CoreCLRCreateCordbObjectName;
+
+            public override unsafe void CreateCordbObject(
+                int iDebuggerVersion,
+                uint pid,
+                void* hmodTargetCLR,
+                void*** ppCordb)
+            {
+                Invoker(iDebuggerVersion, pid, hmodTargetCLR, ppCordb).MaybeThrowHr();
+            }
+
+            public override IntPtr GetFunctionPointer(IntPtr module, string functionName)
+            {
+                return NonWindowsInterop.dlsym(module, functionName);
+            }
+
+            public override IntPtr GetLibrary(string libraryPath)
+            {
+                // Immediate function call binding.
+                const int RTLD_NOW = 0x00002;
+                return NonWindowsInterop.dlopen(libraryPath, RTLD_NOW);
+            }
+        }
+
+        private class MacPlatform : NonWindowsPlatform
+        {
+            public override string ClrModuleName => "libcoreclr.dylib";
+
+            public override string DllName => "libmscordbi.dylib";
+        }
+
+        private class UnixPlatform : NonWindowsPlatform
+        {
+            public override string ClrModuleName => "libcoreclr.so";
+
+            public override string DllName => "libmscordbi.so";
+        }
+
+        private static class WindowsInterop
+        {
+            [DllImport("kernel32", SetLastError = true, CharSet = CharSet.Unicode)]
+            public static extern IntPtr LoadLibraryW(string lpLibFileName);
+
+            [DllImport("kernel32", SetLastError = true, CharSet = CharSet.Ansi)]
+            public static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
         }
 
         private static class NonWindowsInterop
         {
-            [DllImport("libmscordbi", SetLastError = true)]
-            public static extern unsafe int CoreCLRCreateCordbObject(
-                int iDebuggerVersion,
-                uint pid,
-                void* hmodTargetCLR,
-                void*** ppCordb);
+            [DllImport("libc", SetLastError = true, CharSet = CharSet.Ansi)]
+            public static extern IntPtr dlopen(string file, int mode);
+
+            [DllImport("libc", SetLastError = true, CharSet = CharSet.Ansi)]
+            public static extern IntPtr dlsym(IntPtr handle, string name);
         }
     }
 }
